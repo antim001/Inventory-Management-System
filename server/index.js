d07150c974556176050fb008d54f9793edd6e45d
antim001 <antimsarker0@gmail.com>
2025-05-16 21:16:34 +0600

    login functionality is added

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import cors from 'cors'
-
 import connectDB from './db/db.js'
+import auth from './routes/auth.js'
 
 const app=express()
 connectDB()
 const PORT=process.env.PORT || 7000
 app.use(cors());
 app.use(express.json())
+app.use('/api/auth',auth)
 
 
 app.listen(PORT,()=>{
