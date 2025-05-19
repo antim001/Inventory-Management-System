49f5de71007e203f9a101383cb9081da953b99a2
antim001 <antimsarker0@gmail.com>
2025-05-19 21:41:43 +0600

    Categories page is added

---
+++ frontend/src/components/Categories.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const categories = () => {
+    return (<div>
+    <h1>Category Management</h1>
+        <div>
+            <div>
+
+            </div>
+        </div>
+    </div>);
+}
+
+
+export default categories;
