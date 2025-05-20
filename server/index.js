57fa7fa3b90fb7ed2e8159066295e813e2527726
antim001 <antimsarker0@gmail.com>
2025-05-20 17:43:29 +0600

    category route is added

--- server/index.js
+++ server/index.js
@@ -2,13 +2,14 @@ import express from 'express'
 import cors from 'cors'
 import connectDB from './db/db.js'
 import auth from './routes/auth.js'
-
+import categoryRoutes from './routes/category.js'
 const app=express()
 connectDB()
 const PORT=process.env.PORT || 7000
 app.use(cors());
 app.use(express.json())
-app.use('/api/auth',auth)
+app.use('/api/auth',auth);
+app.use('/api/category'.categoryRoutes);
 
 
 app.listen(PORT,()=>{
