a1abfa41edc53312677643199d35420338adcab0
antim001 <antimsarker0@gmail.com>
2025-05-21 17:53:17 +0600

    authmiddleware is added

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import addCategory from '../controllers/categoryController.js'
-
+import authMiddleware from '../middleware/authMiddleware.js'
 const router =express.Router();
 
-router.post('/add',addCategory)
+router.post('/add',authMiddleware,addCategory)
 
 export default router
